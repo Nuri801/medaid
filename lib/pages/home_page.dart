5909ef7b54643d6226bb305054b13a49229fcc8d
SadioMane <nuriddin7612@mail.ru>
2022-12-23 15:58:37 +0900

    Extracting methods!

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:medaid/components/dory_colors.dart';
+import 'package:medaid/pages/today/today_page.dart';
+import 'add/add_page.dart';
+import 'history/history_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  final _pages = [
+    TodayPage(),
+    HistoryPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -12,34 +28,65 @@ class HomePage extends StatelessWidget {
         top: false,
         child: Scaffold(
           appBar: AppBar(),
-          body: Center(),
+          body: _pages[_currentIndex],
           floatingActionButton: FloatingActionButton(
-            onPressed: () {},
+            onPressed: _onAddMedicine,
             child: const Icon(CupertinoIcons.add),
           ),
-          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-          bottomNavigationBar: BottomAppBar(
-            elevation: 0,
-            child: Container(
-              height: kBottomNavigationBarHeight,
-              color: Colors.white,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  CupertinoButton(
-                    onPressed: () {},
-                    child: const Icon(CupertinoIcons.checkmark),
-                  ),
-                  CupertinoButton(
-                    onPressed: () {},
-                    child: const Icon(CupertinoIcons.text_badge_checkmark),
-                  )
-                ],
-              ),
-            ),
-          ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          bottomNavigationBar: _buildBottomAppBar(),
         ),
       ),
     );
   }
+
+  BottomAppBar _buildBottomAppBar() {
+    return BottomAppBar(
+          elevation: 0,
+          child: Container(
+            height: kBottomNavigationBarHeight,
+            color: Colors.white,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                CupertinoButton(
+                  onPressed: () => _onCurrentPage(0),
+                  child: Icon(
+                    CupertinoIcons.checkmark,
+                    color: _currentIndex == 0
+                        ? DoryColors.primaryColor
+                        : Colors.grey,
+                  ),
+                ),
+                CupertinoButton(
+                  onPressed: () => _onCurrentPage(1),
+                  child: Icon(
+                    CupertinoIcons.text_badge_checkmark,
+                    color: _currentIndex == 1
+                        ? DoryColors.primaryColor
+                        : Colors.grey,
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+  }
+
+  void _onCurrentPage(int pageIndex){
+    setState(() {
+      _currentIndex = pageIndex;
+    });
+  }
+
+  void _onAddMedicine () {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const AddPage(),
+      ),
+    );
+  }
+
 }
