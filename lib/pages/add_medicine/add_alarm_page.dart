da4d7a499c7fdda809562783a7da9ac38b5fbfbe
nuri <nuriddin@utec.kr>
2023-02-06 20:42:46 +0900

    Adjusted the page switches;

--- lib/pages/add_medicine/add_alarm_page.dart
+++ lib/pages/add_medicine/add_alarm_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import '../../components/dory_constants.dart';
 
 class AddAlarmPage extends StatelessWidget {
-  const AddAlarmPage({
+  AddAlarmPage({
     Key? key,
     required this.medicineImage,
     required this.medicineName,
@@ -13,26 +13,24 @@ class AddAlarmPage extends StatelessWidget {
   final File? medicineImage;
   final String medicineName;
 
+  late double deviceHeight;
+  late double deviceWidth;
+
   @override
   Widget build(BuildContext context) {
+    deviceHeight = MediaQuery.of(context).size.height;
+    deviceWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: AppBar(
-      ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            medicineImage == null ? Container() : Image.file(medicineImage!),
-            Text(medicineName),
-          ],
-        ),
-      ),
+      appBar: AppBar(),
+      body: pageUI(),
       bottomNavigationBar: SafeArea(
         child: Padding(
           padding: submitButtonBoxPadding,
           child: SizedBox(
             height: submitButtonHeight,
             child: ElevatedButton(
-              onPressed:(){},
+              onPressed: () {},
               style: ElevatedButton.styleFrom(
                 textStyle: Theme.of(context).textTheme.subtitle1,
               ),
@@ -43,4 +41,43 @@ class AddAlarmPage extends StatelessWidget {
       ),
     );
   }
+
+  // Widget pageUI() {
+  //   return SafeArea(
+  //     child: Column(
+  //       children: [
+  //         medicineImage == null ? Container() : Image.file(medicineImage!),
+  //         Text(medicineName),
+  //       ],
+  //     ),
+  //   );
+  // }
+
+  Widget pageUI() {
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+        ),
+        child: Container(
+          height: deviceHeight,
+          width: deviceWidth,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                color: Colors.deepOrange,
+                width: deviceWidth,
+                height: 20,
+              )
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
 }
