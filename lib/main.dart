f216e339b9fd9d2c9ac10bbb475d7564651c86a9
nuri <nuriddin@utec.kr>
2023-03-02 18:23:25 +0900

    Adjusting the widgets;

--- lib/main.dart
+++ lib/main.dart
@@ -26,76 +26,79 @@
 //
 
 // main.dart
-import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+/// The original code for the medaid app is above this line. Everything below this line is experimental.
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// import 'package:flutter/material.dart';
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Remove the debug banner
-      debugShowCheckedModeBanner: false,
-      title: 'KindaCode.com',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  Future<String> getData() async {
-    await Future.delayed(const Duration(seconds: 3));
-    // throw "Error";
-    return 'Big Data';
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: FutureBuilder(
-          future: getData(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            }
-            if (snapshot.hasError) {
-              return Text(snapshot.error.toString());
-            } else {
-              return Scaffold(
-                body: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(snapshot.data.toString()),
-                      ElevatedButton(
-                          onPressed: () {
-                            setState(() {});
-                          },
-                          child: const Text('REFRESH!'))
-                    ],
-                  ),
-                ),
-              );
-            }
-          }),
-
-    );
-  }
-}
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       // Remove the debug banner
+//       debugShowCheckedModeBanner: false,
+//       title: 'KindaCode.com',
+//       theme: ThemeData(
+//         primarySwatch: Colors.amber,
+//       ),
+//       home: const HomeScreen(),
+//     );
+//   }
+// }
+//
+// class HomeScreen extends StatefulWidget {
+//   const HomeScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   State<HomeScreen> createState() => _HomeScreenState();
+// }
+//
+// class _HomeScreenState extends State<HomeScreen> {
+//   Future<String> getData() async {
+//     await Future.delayed(const Duration(seconds: 3));
+//     // throw "Error";
+//     return 'Big Data';
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: FutureBuilder(
+//           future: getData(),
+//           builder: (context, snapshot) {
+//             if (snapshot.connectionState == ConnectionState.waiting) {
+//               return const CircularProgressIndicator();
+//             }
+//             if (snapshot.hasError) {
+//               return Text(snapshot.error.toString());
+//             } else {
+//               return Scaffold(
+//                 body: Center(
+//                   child: Column(
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     children: [
+//                       Text(snapshot.data.toString()),
+//                       ElevatedButton(
+//                           onPressed: () {
+//                             setState(() {});
+//                           },
+//                           child: const Text('REFRESH!'))
+//                     ],
+//                   ),
+//                 ),
+//               );
+//             }
+//           }),
+//
+//     );
+//   }
+// }
 
 
 // class HomeScreen extends StatefulWidget {
@@ -190,3 +193,107 @@ class _HomeScreenState extends State<HomeScreen> {
 //     );
 //   }
 // }
+
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
+import 'dart:async';
+import 'dart:developer' as developer;
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ConnectivityResult _connectionStatus = ConnectivityResult.none;
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    initConnectivity();
+
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+  }
+
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initConnectivity() async {
+    late ConnectivityResult result;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      result = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      developer.log('Couldn\'t check connectivity status', error: e);
+      return;
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+
+    setState(() {
+      _connectionStatus = result;
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Connectivity example app'),
+      ),
+      body: Center(
+          child: Text('Connection Status: ${_connectionStatus.toString()}')),
+    );
+  }o
+
+}
