4e9ff2333f459232c3d1e8db73a5c8e3d5e174ee
nuri <nuriddin@utec.kr>
2023-03-03 17:25:55 +0900

    Adjusting the widgets;

--- lib/main.dart
+++ lib/main.dart
@@ -267,9 +267,6 @@ class _MyHomePageState extends State<MyHomePage> {
       return;
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) {
       return Future.value(null);
     }
