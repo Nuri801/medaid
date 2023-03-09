39c1a40975eb8b1d2784f99ce1ca261b1a4bf189
nuri <nuriddin@utec.kr>
2023-03-09 18:03:57 +0900

    Adjusting the widgets;

--- lib/main.dart
+++ lib/main.dart
@@ -270,11 +270,9 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> _updateConnectionStatus(ConnectivityResult result) async {
-
     setState(() {
       _connectionStatus = result;
     });
-
   }
 
   @override
