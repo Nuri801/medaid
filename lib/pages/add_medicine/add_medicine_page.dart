75e3425f02d5143389dcd4f13f6e34b7fb4384fc
nuri <nuriddin@utec.kr>
2023-03-15 15:06:46 +0900

    Adjusting the widgets;

--- lib/pages/add_medicine/add_medicine_page.dart
+++ lib/pages/add_medicine/add_medicine_page.dart
@@ -182,7 +182,6 @@ class _MedicineImageButtonState extends State<MedicineImageButton> {
   }
 }
 
-
 class PickImageBottomSheet extends StatelessWidget {
   const PickImageBottomSheet(
       {Key? key, required this.onPressedCamera, required this.onPressedGallery})
