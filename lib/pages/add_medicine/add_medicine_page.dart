b15e6167169c1a4473a49cc12cfb4576c7dcf55d
nuri <nuriddin@utec.kr>
2023-03-15 15:03:37 +0900

    Adjusting the widgets;

--- lib/pages/add_medicine/add_medicine_page.dart
+++ lib/pages/add_medicine/add_medicine_page.dart
@@ -163,6 +163,8 @@ class _MedicineImageButtonState extends State<MedicineImageButton> {
     );
   }
 
+
+
   void _onPressed(ImageSource source) {
     Navigator.maybePop(context);
     ImagePicker().pickImage(source: source).then(
