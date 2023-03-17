8d6fef7c73f8a5986596a83edd6cfefae870d1a4
nuri <nuriddin@utec.kr>
2023-03-17 09:11:20 +0900

    Adjusting the widgets;

--- lib/pages/history/history_page.dart
+++ lib/pages/history/history_page.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 class HistoryPage extends StatelessWidget {
   const HistoryPage({Key? key}) : super(key: key);
 
-
-
   @override
   Widget build(BuildContext context) {
 
