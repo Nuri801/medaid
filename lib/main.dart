c8f3b01e282a3e5938ef46364ff96af10d582c21
nuri <nuriddin@utec.kr>
2023-04-14 13:02:05 +0900

    Adjusting the widgets;

--- lib/main.dart
+++ lib/main.dart
@@ -91,17 +91,7 @@ class MyApp extends StatelessWidget {
 // // //       ),
 // // //     );
 
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     return MaterialApp(
-// //       title: 'Flutter Demo',
-// //       theme: ThemeData(
-// //         primarySwatch: Colors.blue,
-// //       ),
-// //       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-// //     );
-// //   }
-// // }
+
 // //
 // // class MyHomePage extends StatefulWidget {
 // //   const MyHomePage({Key? key, required this.title}) : super(key: key);
