a3d846bafdf391aad2a4c76f47467f17e015272a
SadioMane <nuriddin7612@mail.ru>
2023-04-25 14:06:59 +0900

    Adjusting the widgets;

--- lib/main.dart
+++ lib/main.dart
@@ -77,14 +77,7 @@ class MyApp extends StatelessWidget {
 
 
 // //
-// // class MyHomePage extends StatefulWidget {
-// //   const MyHomePage({Key? key, required this.title}) : super(key: key);
-// //
-// //   final String title;
-// //
-// //   @override
-// //   State<MyHomePage> createState() => _MyHomePageState();
-// // }
+
 // //
 // // class _MyHomePageState extends State<MyHomePage> {
 // //   ConnectivityResult _connectionStatus = ConnectivityResult.none;
