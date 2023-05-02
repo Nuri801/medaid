7f7d3945d2a90fc9c0a83b513f9c93d808ccc70a
nuri <nuriddin@utec.kr>
2023-05-02 09:27:13 +0900

    Adjusting the widgets;

--- lib/pages/add_medicine/add_medicine_page.dart
+++ lib/pages/add_medicine/add_medicine_page.dart
@@ -133,8 +133,6 @@ class _MedicineImageButtonState extends State<MedicineImageButton> {
   @override
   Widget build(BuildContext context) {
 
-
-
     return CircleAvatar(
       radius: 40,
       child: CupertinoButton(
