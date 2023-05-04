c7c720f2cf4afbe41fafa8cbe0b1503ca996412c
nuri <nuriddin@utec.kr>
2023-05-04 09:22:09 +0900

    Adjusting the widgets;

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,16 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const Placeholder();
+  }
+}
