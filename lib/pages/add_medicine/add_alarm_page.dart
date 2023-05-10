51554bc8028f2cf17ffd73d70089656e4a523d31
nuri <nuriddin@utec.kr>
2023-05-10 16:45:43 +0900

    Adjusting the widgets;

--- lib/pages/add_medicine/add_alarm_page.dart
+++ lib/pages/add_medicine/add_alarm_page.dart
@@ -17,8 +17,6 @@ class AddAlarmPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // deviceHeight = MediaQuery.of(context).size.height;
-    // deviceWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
       appBar: AppBar(),
@@ -39,6 +37,5 @@ class AddAlarmPage extends StatelessWidget {
         ),
       ),
     );
-
   }
 }
