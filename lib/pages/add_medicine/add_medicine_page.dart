5b25604e991afef9b4ed48e490e256757e9d4702
nuri <nuriddin@utec.kr>
2023-05-22 13:29:40 +0900

    Adjusting the widgets;

--- lib/pages/add_medicine/add_medicine_page.dart
+++ lib/pages/add_medicine/add_medicine_page.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:medaid/components/dory_constants.dart';
